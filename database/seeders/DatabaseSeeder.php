b55a24f6ccc5af35d7c412f8981dfdd81342630b
Faraalif <112565748+Faraalif@users.noreply.github.com>
2024-12-21 14:41:15 +0700

    Update Kecanduan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,39 @@ class DatabaseSeeder extends Seeder
             'tanggal_lahir' => '2001-01-01',
             'password' => bcrypt('password')
         ]);
+
+        
+        // Kecanduan
+        Kecanduan::create([
+            'kode_kecanduan' => 'P01',
+            'nama_kecanduan' => 'Kecanduan Tinggi',
+            'saran_kecanduan' => '<ul>
+                <li>Carilah orang terdekat untuk selalu mengingatkan mengurangi waktu untuk memakai gadget.</li>
+                <li>Fokus pada hal yang ini dicapai dan tujuan hidup.</li>
+                <li>Alihkan perhatian ke aktifitas positif atau mencari berkumpul dengan orang lain.</li>
+            </ul>',
+        ]);
+        Kecanduan::create([
+            'kode_kecanduan' => 'P02',
+            'nama_kecanduan' => 'Kecanduan Sedang',
+            'saran_kecanduan' => '<ul>
+                <li>Lakukan komunikasi dengan orang terdekat dan orang lain.</li>
+                <li>Imbangi dengan aktifitas positif.</li>
+                <li>Memperbanyak kegiatan lagi agar sedikit mengurangi terhadap bermain game online.</li>
+            </ul>',
+        ]);
+        Kecanduan::create([
+            'kode_kecanduan' => 'P03',
+            'nama_kecanduan' => 'Kecanduan Rendah',
+            'saran_kecanduan' => '<ul>
+                <li>Melakukan aktifitas atau kegiatan positif.</li>
+                <li>Menjaga komunikasi.</li>
+                <li>Jaga Kesehatan fisik dan pikiran.</li>
+            </ul>',
+        ]);
+
+
+        // Gejala
+        
     }
 }
