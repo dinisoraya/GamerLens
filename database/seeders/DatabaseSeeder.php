a3bd5698631b0c0377526a0b4041f4ec8bf14ea0
delasylviaa <165258821+delasylviaa@users.noreply.github.com>
2024-12-21 17:41:17 +0700

    basis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -55,7 +55,113 @@ class DatabaseSeeder extends Seeder
             </ul>',
         ]);
 
-
+        // Basis Aturan
+        BasisAturan::insert([
+            [
+                'id' => 16,
+                'kecanduan_id' => 1,
+                'gejala_id' => 1,
+                'value_cf' => 0.80,
+                'created_at' => '2024-12-19 14:24:37',
+                'updated_at' => '2024-12-19 14:24:37'
+            ],
+            [
+                'id' => 17,
+                'kecanduan_id' => 1,
+                'gejala_id' => 3,
+                'value_cf' => 0.80,
+                'created_at' => '2024-12-19 14:24:37',
+                'updated_at' => '2024-12-19 14:24:37'
+            ],
+            [
+                'id' => 18,
+                'kecanduan_id' => 1,
+                'gejala_id' => 5,
+                'value_cf' => 0.80,
+                'created_at' => '2024-12-19 14:24:37',
+                'updated_at' => '2024-12-19 14:24:37'
+            ],
+            [
+                'id' => 19,
+                'kecanduan_id' => 1,
+                'gejala_id' => 9,
+                'value_cf' => 0.80,
+                'created_at' => '2024-12-19 14:24:37',
+                'updated_at' => '2024-12-19 14:24:37'
+            ],
+            [
+                'id' => 20,
+                'kecanduan_id' => 1,
+                'gejala_id' => 10,
+                'value_cf' => 0.80,
+                'created_at' => '2024-12-19 14:24:37',
+                'updated_at' => '2024-12-19 14:24:37'
+            ],
+            [
+                'id' => 21,
+                'kecanduan_id' => 1,
+                'gejala_id' => 13,
+                'value_cf' => 0.80,
+                'created_at' => '2024-12-19 14:24:37',
+                'updated_at' => '2024-12-19 14:24:37'
+            ],
+            [
+                'id' => 22,
+                'kecanduan_id' => 2,
+                'gejala_id' => 6,
+                'value_cf' => 0.40,
+                'created_at' => '2024-12-19 14:26:56',
+                'updated_at' => '2024-12-19 14:26:56'
+            ],
+            [
+                'id' => 23,
+                'kecanduan_id' => 2,
+                'gejala_id' => 8,
+                'value_cf' => 0.40,
+                'created_at' => '2024-12-19 14:26:56',
+                'updated_at' => '2024-12-19 14:26:56'
+            ],
+            [
+                'id' => 24,
+                'kecanduan_id' => 2,
+                'gejala_id' => 12,
+                'value_cf' => 0.40,
+                'created_at' => '2024-12-19 14:26:56',
+                'updated_at' => '2024-12-19 14:26:56'
+            ],
+            [
+                'id' => 25,
+                'kecanduan_id' => 2,
+                'gejala_id' => 14,
+                'value_cf' => 0.40,
+                'created_at' => '2024-12-19 14:26:56',
+                'updated_at' => '2024-12-19 14:26:56'
+            ],
+            [
+                'id' => 26,
+                'kecanduan_id' => 3,
+                'gejala_id' => 11,
+                'value_cf' => 0.20,
+                'created_at' => '2024-12-19 14:28:33',
+                'updated_at' => '2024-12-19 14:28:33'
+            ],
+            [
+                'id' => 27,
+                'kecanduan_id' => 3,
+                'gejala_id' => 2,
+                'value_cf' => 0.20,
+                'created_at' => '2024-12-19 14:28:33',
+                'updated_at' => '2024-12-19 14:28:33'
+            ],
+            [
+                'id' => 28,
+                'kecanduan_id' => 3,
+                'gejala_id' => 4,
+                'value_cf' => 0.20,
+                'created_at' => '2024-12-19 14:28:33',
+                'updated_at' => '2024-12-19 14:28:33'
+            ]
+        ]);
         // Gejala
         
     }
