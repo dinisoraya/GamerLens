0440a5aaba315690dbce1229562f0b6196afe8a9
Atikah Oktaria <112566355+AtikahOktaria@users.noreply.github.com>
2024-12-21 19:07:19 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -163,6 +163,103 @@ class DatabaseSeeder extends Seeder
             ]
         ]);
         // Gejala
-        
+        Gejala::create([
+           'id' => '1',
+            'kode_gejala' => 'G01',
+            'nama_gejala' => 'Sulit berkonsentrasi saat belajar',
+            'created_at' => '2024-12-19 21:08:40',
+            'updated_at' => '2024-12-19 21:08:40',
+        ]);
+        Gejala::create([
+            'id' => '2',
+            'kode_gejala' => 'G02',
+            'nama_gejala' => 'Sering mengakses game di waktu luang',
+            'created_at' => '2024-12-19 21:09:09',
+            'updated_at' => '2024-12-19 21:09:09,'
+        ]);
+        Gejala::create([
+            'id' => '3',
+            'kode_gejala' => 'G03',
+            'nama_gejala' => 'Malas jika disuruh mengerjakan sesuatu selain bermain game',
+            'created_at' => '2024-12-19 21:13:30',
+            'updated_at' => '2024-12-19 21:13:30',
+        ]);
+        Gejala::create([
+            'id' => '4',
+            'kode_gejala' => 'G04',
+            'nama_gejala' => 'Selalu meluangkan waktunya untuk bermain game',
+            'created_at' => '2024-12-19 21:14:00',
+            'updated_at' => '2024-12-19 21:14:00',
+        ]);
+        Gejala::create([
+            'id' => '5',
+            'kode_gejala' => 'G05',
+            'nama_gejala' => 'Bermain game sampai lupa waktu',
+            'created_at' => '2024-12-19 21:14:18',
+            'updated_at' => '2024-12-19 21:14:18',
+        ]);
+        Gejala::create([
+             'id' => '6',
+            'kode_gejala' => 'G06',
+            'nama_gejala' => 'Gelisah jika tidak bermain game',
+            'created_at' => '2024-12-19 21:14:34',
+            'updated_at' => '2024-12-19 21:14:34',
+        ]);
+        Gejala::create([
+            'id' => '7',
+            'kode_gejala' => 'G07',
+            'nama_gejala' => 'Terus menerus memikiran tentang game',
+            'created_at' => '2024-12-19 21:15:04',
+            'updated_at' => '2024-12-19 21:15:04',
+        ]);
+        Gejala::create([
+            'id' => '8',
+            'kode_gejala' => 'G08',
+            'nama_gejala' => 'Suka menyendiri di suatu tempat',
+            'created_at' => '2024-12-19 21:15:28',
+            'updated_at' => '2024-12-19 21:15:28',
+        ]);
+        Gejala::create([
+            'id' => '9',
+            'kode_gejala' => 'G09',
+            'nama_gejala' => 'Tidak tertarik untuk bergaul dengan lingkungan sekitar',
+            'created_at' => '2024-12-19 21:15:52',
+            'updated_at' => '2024-12-19 21:15:52',
+        ]);
+        Gejala::create([
+            'id' => '10',
+            'kode_gejala' => 'G10',
+            'nama_gejala' => 'Rela mengeluarkan banyak uang demi game',
+            'created_at' => '2024-12-19 21:16:22',
+            'updated_at' => '2024-12-19 21:16:22',
+        ]);
+        Gejala::create([
+            'id' => '11',
+            'kode_gejala' => 'G11',
+            'nama_gejala' => 'Pola hidup yang tidak teratur',
+            'created_at' => '2024-12-19 21:16:40',
+            'updated_at' => '2024-12-19 21:16:40',
+        ]);
+        Gejala::create([
+            'id' => '12',
+            'kode_gejala' => 'G12',
+            'nama_gejala' => 'Jika diajak berbicara selalu tentang game',
+            'created_at' => '2024-12-19 21:17:05',
+            'updated_at' => '2024-12-19 21:17:05',
+        ]);
+        Gejala::create([
+            'id' => '13',
+            'kode_gejala' => 'G13',
+            'nama_gejala' => 'Sering berhalusinasi',
+            'created_at' => '2024-12-19 21:17:25',
+            'updated_at' => '2024-12-19 21:17:25',
+        ]);
+        Gejala::create([
+            'id' => '14',
+            'kode_gejala' => 'G14',
+            'nama_gejala' => 'Menganggap Game Sebagai Teman Terbaik',
+            'created_at' => '2024-12-19 21:17:44',
+            'updated_at' => '2024-12-19 21:17:44',
+        ]);
     }
 }
